1bcd6f0290f5ec3da23be5d2e41b544713e1ddf0
EzioAetos <semvaguila@yahoo.com>
2024-12-13 01:47:42 +1100

    Added totem of togetherness functionality

--- behaviour_pack/scripts-dev/loops/totem_of_togetherness.ts
+++ behaviour_pack/scripts-dev/loops/totem_of_togetherness.ts
@@ -1,49 +1,31 @@
-import {system, world, Player, EffectType, ItemStack, EquipmentSlot, EntityComponentTypes } from "@minecraft/server";
-import { MinecraftEffectTypes,MinecraftItemTypes} from '@minecraft/vanilla-data';
+import {system, world, Player, EquipmentSlot, EntityComponentTypes} from "@minecraft/server";
+import { MinecraftEffectTypes} from '@minecraft/vanilla-data';
 
 
-const Bottle = "amethyst:totem_of_togetherness";
+
 const healthboost = MinecraftEffectTypes.HealthBoost;
-const range = 16;
 
-function nearbyplayercount(player: Player, range: number): number {
+
+function togetherness(player: Player): void {
     const position = player.location;
-    return Array.from(world.getPlayers()).filter((p) => {
-        if (p === player) return false;
-        const distance = Math.sqrt(
-            Math.pow(p.location.x - position.x, 2) +
-            Math.pow(p.location.y - position.y, 2) +
-            Math.pow(p.location.z - position.z, 2)
-        );
-        return distance <= range;
-    }).length;
+    const equippable = player.getComponent(EntityComponentTypes.Equippable)
+    const offhand = equippable?.getEquipment(EquipmentSlot.Offhand)
+    const mainhand = equippable?.getEquipment(EquipmentSlot.Mainhand)
+    if (offhand?.hasComponent("amethyst:togetherness") || mainhand?.hasComponent("amethyst:togetherness") ) {
+        const uniqueplayerslist = player.dimension.getPlayers({location: position, maxDistance: 16 })
+        player.addEffect(healthboost, 1, {amplifier: uniqueplayerslist.length, showParticles: false})
+    }
+
 }
 
-
-world.beforeEvents.worldInitialize.subscribe(() => {
-    world.getPlayers().forEach((player) => {
-        const inventory = player.getComponent("inventory");
-        if (!inventory) return;
-
-        const hasBottle = inventory.container?.any((item) => item?.typeId === Bottle);
-        if (hasBottle) {
-            const nearbyPlayers = nearbyplayercount(player, range);
-            const togethernesslevel = Math.min(nearbyPlayers + 1, 10); 
-            player.addEffect(healthboost, 20, togethernesslevel, false); 
-        } else {
-            player.removeEffect(healthboost); 
-        }
-    });
-});
-
-export default function load_bottle_o_together() {
+export default function load_totem_o_togetherness() {
     system.runInterval(() => {
         let playerlist = world.getPlayers();
     
         playerlist.forEach((player) => {
-            nearbyplayercount(player)
+            togetherness(player)
         });
-    }, 20)
+    }, 1)
 
     console.log('[Loops] Loaded Bottle Of Togetheness Loop') 
 }
