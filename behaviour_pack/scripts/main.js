7d3c9d93044798cc756a264a530ba455cfdeeefe
Pavel <pbkrutov@gmail.com>
2025-01-02 17:19:48 +0200

    bump and bundle version

--- behaviour_pack/scripts/main.js
+++ behaviour_pack/scripts/main.js
@@ -7776,7 +7776,7 @@ function load_world_event_handlers(guild_id2) {
 }
 
 // behaviour_pack/scripts-dev/main.ts
-var guild_id = "1213827104945471538";
+var guild_id = "611008530077712395";
 load_loops();
 load_custom_components();
 load_world_event_handlers(guild_id);
