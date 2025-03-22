b88d227d05aae3d8a026af618210cbb602f3a860
Pavel <pbkrutov@gmail.com>
2025-03-22 22:34:10 +0200

    add the sacrifice component

--- behaviour_pack/scripts-dev/components/index.ts
+++ behaviour_pack/scripts-dev/components/index.ts
@@ -1,7 +1,9 @@
 import load_fungus_spreading_component from "./fungus_spread";
 import load_glitch_component from "./glitch";
+import load_altar_component from "./sacrifice";
 
 export default function load_custom_components() {
     load_fungus_spreading_component()
     load_glitch_component()
+    load_altar_component()
 }
---
+++ behaviour_pack/scripts-dev/components/sacrifice.ts
@@ -0,0 +1,49 @@
+import {
+    BlockComponentRandomTickEvent,
+    Player,
+    world,
+    BlockComponentPlayerInteractEvent, MolangVariableMap
+} from "@minecraft/server";
+import utils from "../utils";
+
+const molang = new MolangVariableMap();
+molang.setColorRGB("variable.color", { red: 0, green: 0.619, blue: 0.376 });
+
+
+export default function load_altar_component() {
+    function sacrifice(event : BlockComponentPlayerInteractEvent) {
+        if (Math.random() < 0.7 && event.player) {
+            utils.commands.send_message(event.dimension.id, event.player?.name, 'You have Sacrificed')
+        }
+    }
+
+    function particles(event : BlockComponentRandomTickEvent) {
+        if (event.block.isValid()) {
+            const location = event.block.location;
+            const radius = 3;
+
+            let random_location = {
+                x: location.x + (Math.floor(Math.random() * radius) * (Math.random() < 0.5 ? -1 : 1)),
+                y: location.y + Math.floor(Math.random() * 4),
+                z: location.z + (Math.floor(Math.random() * radius) * (Math.random() < 0.5 ? -1 : 1))
+            };
+
+            if (event.block.dimension.getBlock(random_location)) {
+                event.dimension.spawnParticle("minecraft:colored_flame_particle", random_location, molang)
+            }
+        }
+    }
+
+    world.beforeEvents.worldInitialize.subscribe(initEvent => {
+        initEvent.blockComponentRegistry.registerCustomComponent('amethyst:sacrifice',
+            {
+                onRandomTick(event) {
+                    particles(event)
+                },
+                onPlayerInteract(event) {
+                    sacrifice(event)
+                }
+            }
+        )
+    })
+}
