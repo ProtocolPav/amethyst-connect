3816279983c0c0676e39f09f14f97ef685144b17
Pavel <pbkrutov@gmail.com>
2025-08-03 20:12:16 +0300

    add new emoji

--- behaviour_pack/scripts-dev/events/chat.ts
+++ behaviour_pack/scripts-dev/events/chat.ts
@@ -8,39 +8,11 @@ export default function load_chat_handler() {
         const gamertag = chat_event.sender.name
         const thorny_user = api.ThornyUser.fetch_user(gamertag)
 
-        if (chat_event.message.startsWith('!lore')) {
-            const equippable = chat_event.sender.getComponent(EntityComponentTypes.Equippable)
-            const mainhand = equippable?.getEquipment(EquipmentSlot.Mainhand)
-            system.run(() => {
-                switch (chat_event.message.split(' ')[1].toLowerCase()) {
-                    case 'add':
-                        if (mainhand) {
-                            const lore = mainhand.getLore()
-                            lore.push(chat_event.message.split('!lore add ')[1])
-                            mainhand.setLore(lore)
-                            equippable?.setEquipment(EquipmentSlot.Mainhand, mainhand)
-                        }
-                        break;
+        world.sendMessage({
+            rawtext: [{text: `${thorny_user?.get_role_display()} §7${gamertag}:§r ${chat_event.message}`}],
+        });
 
-                    case 'remove':
-                        if (mainhand) {
-                            mainhand.setLore([])
-                            equippable?.setEquipment(EquipmentSlot.Mainhand, mainhand)
-                        }
-                        break;
-
-                    default:
-                        chat_event.sender.sendMessage('The command is !lore [add|remove] [your_lore]')
-                }
-            })
-        }
-        else {
-            world.sendMessage({
-                rawtext: [{text: `§l§8[§r${thorny_user?.get_role_display()}§l§8]§r §7${gamertag}:§r ${chat_event.message}`}],
-            });
-
-            system.run(() => { api.Relay.message(gamertag, chat_event.message) });
-        }
+        system.run(() => { api.Relay.message(gamertag, chat_event.message) });
 
         chat_event.cancel = true;
     });
